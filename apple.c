955564d5599c44b18ec60d342228060b8d5ef19e
Lifestory8000 <3550936496@qq.com>
2026-09-17 17:14:17 +0800

    apple

--- apple.c
+++ apple.c
@@ -1,10 +1,11 @@
 //如果你想存放单词 “apple”，使用一个char类型的字符变量能实现吗？尝试编程验
 // 证你的想法，并思考正确的存储方式应该是什么？
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
     char name[20];
-    strcpy(name, 20, "apple");
+    strcpy(name, "apple");
     printf("%s", name);
 
 
