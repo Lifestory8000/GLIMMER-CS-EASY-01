1aaa5a8c5c14372717f1283e8e78b10ee9348ada
Lifestory8000 <3550936496@qq.com>
2026-09-17 17:56:54 +0800

    dowhile

---
+++ do while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int a=0 ;   
+        int i=1;
+        do{
+            a=a+i;
+            i++;}while(i<11);
+        printf("%d", a);
+
+
+
+    return 0;
+}
