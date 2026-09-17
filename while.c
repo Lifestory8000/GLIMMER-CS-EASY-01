642db9a476d37d9788a2ef0351e4e82e076b8963
Lifestory8000 <3550936496@qq.com>
2026-09-17 17:52:46 +0800

    while

---
+++ while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int a=0 ;   
+        int i=1;
+        while(i<11){
+            a=a+i;
+            i++;
+            
+
+        
+        }
+        printf("%d", a);
+
+
+
+    return 0;
+}
